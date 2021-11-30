249fabf972449a5085be912e1e3f3d29eb29087c
jessamynsmith <geekchick77@gmail.com>
2021-11-30 14:04:19 -0700

    Changed chat page to a class-based view that requires login. Added room and username to chat page.

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,9 +1,22 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 def index(request):
     return render(request, 'chat/index.html')
+
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+
+class RoomView(LoginRequiredMixin, TemplateView):
+    template_name = 'chat/room.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['room_name'] = self.kwargs.get('room_name')
+        return context_data
